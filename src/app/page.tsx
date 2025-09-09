cbe542423fa561763d9c5b17f247b19abc7708f1
Rhuan Guilherme <rhuan23032004@gmail.com>
2025-09-09 16:17:11 -0300

    Ajustando Title e Description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+export const metadata = {
+  title: 'Pompolimpíadas 2024 | Ginástica Íntima Feminina',
+  description:
+    'Participe da superaula gratuita e aprenda Ginástica Íntima para saúde, autoestima e prazer. Inscreva-se nas Pompolimpíadas 2024.',
+  keywords:
+    'Ginástica Íntima, Pompolimpíadas, saúde feminina, exercícios íntimos',
+};
+
 import ContactSection from '@/components/sections/ContactSection';
 import FinalSection from '@/components/sections/FinalSection';
 import FooterSection from '@/components/sections/FooterSection';
 import HeroSection from '@/components/sections/HeroSection';
 import MidCenterSection from '@/components/sections/MidCenterSection';
 import MidSection from '@/components/sections/MidSection';
+import Head from 'next/head';
 
 export default function Home() {
   return (
